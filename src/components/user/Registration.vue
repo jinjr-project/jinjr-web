cc70df3128622d56261df9d67d5a3ee1b086f02e
Jinjr <jinjr.mi@gmail.com>
2019-03-18 23:08:29 +0800

    思考框架

---
+++ src/components/user/Registration.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<router-link to="/user/login">登录</router-link>
+	</div>
+</template>
+<script>
+	export default {
+		methods: {
+			register() {
+				this.$router.push("/project/new")
+			}
+		}
+	}
+</script>
