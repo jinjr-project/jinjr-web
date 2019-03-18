1c99d96dddfdc138dceeb36394d689c07b8100ce
Jinjr <jinjr.mi@gmail.com>
2019-03-19 07:28:44 +0800

    面包屑导航

--- src/components/collaboration/Collaboration.vue
+++ src/components/collaboration/Collaboration.vue
@@ -1,4 +1,19 @@
 <template>
-	<div>
-	</div>
+  <el-container>
+    <el-row>
+      <el-col>
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">项目</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">Jinjr</a></el-breadcrumb-item>
+          <el-breadcrumb-item>Jinjr 面板</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <h1>待办事项列表</h1>
+      </el-col>
+    </el-row>
+  </el-container>
+
 </template>
